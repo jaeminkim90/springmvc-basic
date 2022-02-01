89338d5f2e1e62699b76ba176ddbb94a922419e9
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-02 07:39:00 +0900

    FEAT: 요청 바디 메시지에 담긴 데이터를 InputStream로 받는 예제 코드 구현

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,2 +1,43 @@
-package hello.springmvc.basic.request;public class RequestBodyStringController {
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        response.getWriter().write("OK!!");
+    }
+
+
+    /**
+     * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
+     * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyString(InputStream inputStream, Writer responseWriter) throws IOException {
+
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+        responseWriter.write("OK!!");
+    }
 }
