ae53e3fe1f6f17fb61f71c0932b1bb82c1f64082
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-02 12:46:51 +0900

    REFACTOR: @RequestBody를 사용하여 JSON 데이터를 파라미터에서 객체로 바로 받을 수 있는 예제 코드 구현

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -53,4 +53,18 @@ public class RequestBodyJsonController {
         log.info("username={}, age={}", data.getUsername(), data.getAge());
         return "ok";
     }
+
+    /**
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (content-
+    type: application/json)
+     *
+     */
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
 }
